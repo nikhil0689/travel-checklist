6e0fbec7734682b8a35af40d674f8937ef85bd2f
nikhil <nikhil0689@gmail.com>
2024-03-23 13:32:03 -0700

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import "../index.css";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleDeleteAll() {
+    setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onItemToggle={handleToggleItem}
+        onDeleteAll={handleDeleteAll}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
